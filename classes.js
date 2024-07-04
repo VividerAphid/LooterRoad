a91426ae7d40f9e4d22c6e359f9ae9642aad69cf
VividerAphid <trever.kress@gmail.com>
2024-07-04 17:33:11 -0400

    Working Inventory class

--- classes.js
+++ classes.js
@@ -7,13 +7,11 @@ class Chest{
 }
 
 class Item{
-    constructor(id, tId, value, tags, stats){
-        //tId from template
+    constructor(id, tId){
+        //tId is pointer to itemTemplate
         this.id = id;
         this.tId = tId;
-        this.value = value;
-        this.tags = tags;
-        if(stats){this.stats = stats;}
+        this.name = tId.name;
     }
 
 }
@@ -45,11 +43,78 @@ class ChestTemplate{
 }
 
 class ItemTemplate{
-    constructor(id, name, value, tags, stats){
+    constructor(id, name, value, stackSize, tags, stats){
         this.id = id;
         this.name = name;
         this.value = value;
+        this.stackSize = stackSize;
         this.tags = tags;
         if(stats){this.stats = stats;}
+    }
+}
+
+class Character{
+    constructor(id, name, inventory){
+        this.id = id;
+        this.name = name;
+        this.inventory = inventory;
+    }
+}
+
+class Inventory{
+    constructor(maxSlots){
+        this.maxSlots = maxSlots;
+        this.items = [];
+    }
+    addItem(item, count){
+        if(this.checkOpenStacks(item)){
+            console.log("Adding to stack");
+            let stack = this.items[this.findOpenStack(item)];
+            stack.amount += count;
+            return true;
+        }
+        else{
+            if(this.checkOpenSlots()){
+                console.log("New stack");
+                this.items.push({itemId: item.tId.id, amount:count});
+                return true;
+            }
+            else{
+                console.log("Inventory full!");
+                return false;
+            }
+        }
+    }
+    removeItem(){
+
+    }
+    checkOpenSlots(){
+        //find if there are any more open slots
+        if(this.items.length < this.maxSlots){
+            return true;
+        }
+        return false;
+    }
+    checkOpenStacks(item){
+        //find if there is an open stack for this item
+        for(let r = 0; r < this.items.length; r++){
+            if(this.items[r].itemId == item.tId.id){
+                if(this.items[r].amount < item.tId.stackSize){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    findOpenStack(item){
+        //return index of open stack
+        for(let r = 0; r < this.items.length; r++){
+            if(this.items[r].itemId == item.tId.id){
+                if(this.items[r].amount < item.tId.stackSize){
+                    return r;
+                }
+            }
+        }
+        return false;
     }
 }
